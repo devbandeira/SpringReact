96b53dcc1b7f01716d25afd8a3dfd0b4c60887b1
devbandeira <dev.bandeira@outlook.com>
2022-07-13 00:07:36 -0300

    Project Created

---
+++ backend/src/main/java/com/devbandeira/dsmeta/ProjetoSpringEReactApplication.java
@@ -0,0 +1,13 @@
+package com.devbandeira.dsmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringEReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringEReactApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devbandeira/dsmeta/ProjetoSpringEReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devbandeira.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringEReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
